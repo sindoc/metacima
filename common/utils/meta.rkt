87c0973eb36d3b95ce9fe4be26e6ab96e5f5ff27
Sina Khakbaz Heshmati <sina@khakbaz.com>
2012-05-26 21:27:07 +0200

    Improves (very slightly) the code composition; serving programmer's mood again

--- common/utils/meta.rkt
+++ common/utils/meta.rkt
@@ -1,30 +1,19 @@
 #lang racket
 
-(require
- metacima/common/utils/base
- metacima/common/utils/debug)
+(require "base.rkt")
 
-(provide keyword-apply-)
+(provide (all-defined-out))
 
 (define (keyword-apply- proc pool)
   
-  (define (comparator a b)
-    (dbg 'comparator a b)
-    (string<? 
-     (keyword->string (car a))
-     (keyword->string (car b))))
-  
   (define sorted-pool 
-    (sort pool comparator))
+    (sort pool (λ (a b)
+                 (keyword<? (car a) (car b)))))
   
   (define kws (map car sorted-pool))
   (define non-kws (map cadr sorted-pool))
-  
   (define kws-count (length kws))
   (define kargs (take non-kws kws-count))
   (define pargs (drop non-kws kws-count))
-
-  (dbg 'keyword-apply- proc pool 
-       sorted-pool kws non-kws kws-count kargs pargs)
   
   (keyword-apply proc kws kargs pargs))
