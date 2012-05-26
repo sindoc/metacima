b02bd8139dcf04ce197255a7fe86e1fdb7a68459
Sina Khakbaz Heshmati <sina@khakbaz.com>
2012-05-26 21:23:54 +0200

    Adds a few helper functions

--- common/utils/base.rkt
+++ common/utils/base.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(require
+ metacima/common/syntax)
+
 (provide
  (all-defined-out))
 
@@ -11,20 +14,23 @@
 
 (define (map* n lst proc)
   (define lst-length (length lst))
-  (unless (zero? (remainder lst-length n))
-    (error 
-     (string-append
-      "map*: length of list should be a"
-      " multiple of chunk size n:") 
-     n lst-length lst))
-  (let iter
-    ((partial-lst lst))
-    (cond
-      ((null? partial-lst) partial-lst)
-      (else
-       (cons 
-        (apply proc (take partial-lst n))
-        (iter (drop partial-lst n)))))))
+  (cond 
+    ((null? lst) lst)
+    (else
+     (unless (zero? (remainder lst-length n))
+       (error 
+        (string-append
+         "map*: length of list should be a"
+         " multiple of chunk size n:") 
+        n lst-length lst))
+     (let iter
+       ((partial-lst lst))
+       (cond
+         ((null? partial-lst) partial-lst)
+         (else
+          (cons 
+           (apply proc (take partial-lst n))
+           (iter (drop partial-lst n)))))))))
 
 (define (show . args)
   (for-each
@@ -36,3 +42,14 @@
 
 (define (zip lst (n 2))
   (map* n lst (λ args args)))
+
+(define (map+ proc lst)
+  (let ((i 0))
+    (map
+     (λ (e)
+       (inc i 1)
+       (proc i e))
+     lst)))
+
+(define (force-thunks thunks . args)
+  (for-each (λ (thunk) (apply thunk args)) thunks))
